948fafaea411f2c6661d306a2980083f1836b005
craig <craig@192.168.0.122>
2022-12-01 14:30:41 +0000

    Management button

--- src/main/java/com/mycompany/videorental/MainApp.java
+++ src/main/java/com/mycompany/videorental/MainApp.java
@@ -43,6 +43,8 @@ public class MainApp extends javax.swing.JFrame {
         Icon = new javax.swing.JLabel();
         Email = new javax.swing.JLabel();
         AccountStatus = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        Management = new javax.swing.JButton();
         Subscribe = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -62,7 +64,7 @@ public class MainApp extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Length", "Movie Name", "Genre", "Description", "Reating"
+                "Length", "Movie Name", "Genre", "Description", "Rating"
             }
         ) {
             Class[] types = new Class [] {
@@ -120,6 +122,11 @@ public class MainApp extends javax.swing.JFrame {
 
         AccountStatus.setText("jLabel2");
 
+        jLabel1.setText("Watch History");
+
+        Management.setBackground(new java.awt.Color(0, 204, 255));
+        Management.setText("Manage");
+
         javax.swing.GroupLayout AccountLayout = new javax.swing.GroupLayout(Account);
         Account.setLayout(AccountLayout);
         AccountLayout.setHorizontalGroup(
@@ -131,11 +138,17 @@ public class MainApp extends javax.swing.JFrame {
                     .addGroup(AccountLayout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(AccountLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(AccountName, javax.swing.GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
-                            .addComponent(Email, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(AccountStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGroup(AccountLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(AccountLayout.createSequentialGroup()
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(AccountLayout.createSequentialGroup()
+                                .addGroup(AccountLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(AccountName, javax.swing.GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
+                                    .addComponent(Email, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(AccountStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Management, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                 .addContainerGap())
         );
         AccountLayout.setVerticalGroup(
@@ -144,16 +157,19 @@ public class MainApp extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(AccountLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(AccountLayout.createSequentialGroup()
-                        .addComponent(AccountName, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(AccountLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(AccountName, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Management))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(Email)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(AccountStatus)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1))
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         MainTabs.addTab("Account", Account);
@@ -316,10 +332,12 @@ public class MainApp extends javax.swing.JFrame {
     private javax.swing.JLabel Email;
     private javax.swing.JLabel Icon;
     private javax.swing.JTabbedPane MainTabs;
+    private javax.swing.JButton Management;
     private javax.swing.JPanel Subscribe;
     private javax.swing.JTable VideoTable;
     private javax.swing.JPanel Videos;
     private javax.swing.JTextArea WatchHistory;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
